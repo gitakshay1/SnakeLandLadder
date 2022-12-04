dfd0bb60bba6b7e14113f572d45db931c641f1e7
gitakshay1 <akshaypatil309p8@gmail.com>
2022-12-04 22:41:37 +0530

    [Akshay]-Add case of No play, Snake And Ladder

--- SnakeAndLadder/Program.cs
+++ SnakeAndLadder/Program.cs
@@ -9,14 +9,33 @@ namespace Snake_Ladder
             for (int Position = 0; Position <= 100;)
             {
             Here:
-                Random rnd= new Random();
+                Random Num = new Random();
+                Random Opt = new Random();
                 Console.WriteLine("Your Position is " + Position);
-                Console.WriteLine("Enter to rolls the Die");
+                Console.WriteLine("\nEnter to rolls the Die");
                 Console.ReadLine();
-                int Num_on_Die = rnd.Next(1,6);
+                int Num_on_Die = Num.Next(1,6);
+                int Option=Opt.Next(0,3);
                 Console.WriteLine("Die Number is " + Num_on_Die);
-                Position = Position + Num_on_Die;
-           
+                switch (Option)
+                {
+                    case 0:
+                        Console.WriteLine("Its No Play");
+                        break;
+                    case 1:
+                        Console.WriteLine("Its Ladder");
+                        Position = Position + Num_on_Die;
+                        break;
+                    case 2:
+                        Console.WriteLine("Its Snake");
+                        Position = Position - Num_on_Die;
+                        if(Position <= 0)
+                        {
+                            Position= 0;
+                        }
+                        break;
+                }
+       
                 if (Position >= 101)
                 {
                     Position = Position - Num_on_Die;
@@ -28,7 +47,7 @@ namespace Snake_Ladder
                 }
 
             }
-            Console.WriteLine("Your Position is 100 \n You Won ");
+            Console.WriteLine("\nYour Position is 100 \n You Won ");
 
         }
     }
